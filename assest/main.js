d3d6428bb1e1975f30f8208ae09ca0c0b85d797e
Ahmed3nter <samyadham34@gmail.com>
2024-12-14 23:54:22 +0200

    Add files via upload

---
+++ assest/main.js
@@ -0,0 +1,13 @@
+function mark() {
+    image.src =
+      "https://upload.wikimedia.org/wikipedia/commons/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg";
+    document.getElementById("name").innerHTML = "Name: Mark Zuckerberg";
+    document.getElementById("pos").innerHTML = "Position: CEO OF Meta";
+  }
+  function steve() {
+    image.src =
+      "https://static.toiimg.com/thumb/msid-86749110,width-400,resizemode-4/86749110.jpg";
+    document.getElementById("name").innerHTML = "Name: Steve Jobs";
+    document.getElementById("pos").innerHTML = "Position: CEO OF Apple";
+  }
+  
